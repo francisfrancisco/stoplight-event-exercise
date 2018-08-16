74cae214d4992f2f488e1255af6f7ed23ab4b185
Francis Francisco <francis.francisco44@gmail.com>
2018-08-16 13:19:15 -0700

    commented out some code in js file

--- index.js
+++ index.js
@@ -8,22 +8,22 @@
   const stopLight = document.getElementById("stopLight");
   redButton.addEventListener("click", function(){
     stopLight.classList.toggle("stop")
-    goLight.classList.remove('go')
-    slowLight.classList.remove('slow')
+    // goLight.classList.remove('go')
+    // slowLight.classList.remove('slow')
   });
   const yellowButton = document.getElementById("slowButton");
   const slowLight = document.getElementById('slowLight');
   yellowButton.addEventListener('click', function(){
     slowLight.classList.toggle('slow')
-    stopLight.classList.remove('stop')
-    goLight.classList.remove('go')
+    // stopLight.classList.remove('stop')
+    // goLight.classList.remove('go')
   })
   const goButton = document.getElementById('goButton');
   const goLight = document.getElementById('goLight');
   goButton.addEventListener('click', function(){
     goLight.classList.toggle('go');
-    stopLight.classList.remove('stop')
-    slowLight.classList.remove('slow')
+    // stopLight.classList.remove('stop')
+    // slowLight.classList.remove('slow')
   })
   redButton.addEventListener('mouseover', function(){
     console.log("Entered Stop button");
@@ -44,6 +44,19 @@
     console.log('Left Go Button');
   })
 
+  function buttonTouch(){
+    const bulbs = document.querySelectorAll(".bulb")
+    const buttons = document.querySelectorAll('.button')
+    for(let i=0; i<buttons.length; i++){
+      buttons[i].addEventListener('click', function(){
+        if(bulbs[i].classList.contains('go') || bulbs[i].classList.contains('slow') || bulbs[i].classList.contains('stop')){
+          console.log(`${buttons[i].innerText} bulb on`)
+        } else {
+          console.log(`${buttons[i].innerText} bulb off`)
+        }
+      })
+    }
+  }
   const bulbs = document.querySelectorAll(".bulb")
   const buttons = document.querySelectorAll('.button')
   for(let i=0; i<buttons.length; i++){
